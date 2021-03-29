d0682a4e240465899e6c2581377ad0ca5f17889c
Oliver Warren <66948369+o-s-warren@users.noreply.github.com>
2021-03-29 14:54:36 +0100

    Add static about page

---
+++ page-about.php
@@ -0,0 +1,41 @@
+<?php
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+
+
+
+<!-- Body Section -->
+<!-- About the Blog -->
+<div class="about">
+  <div class="about-text">
+
+  <h2>
+    About the Blog
+  </h2>
+  <p><br>The Fitzwilliam Museum has studios for the conservation of antiquities, applied arts, manuscripts and printed books, and works of art on paper and a scientific laboratory to carry out analysis on the collection. The Museum’s paintings are conserved at The Hamilton Kerr Institute, which is also a department of the Museum.</p>
+
+<p>Conservators at the Fitzwilliam Museum work alongside curators, researching and investigating the materials and technology of the objects. They assess and record condition and undertake conservation treatment where necessary. The other important part of conservation work is collections care. This includes all the actions taken to avoid damage and slow down deterioration of the objects by controlling the environment around them.</p>
+   </div>
+  <div class="about-image">
+  </div>
+</div>
+<!-- Contact Us -->
+<div class="contact-us">
+  <h3>
+    Contact Us
+  </h3>
+  <p>
+    Flavia Ravaioli (<a href="#">fr306@cam.ac.uk</a>) and Helena Rodwell (<a href="#">hjr41@cam.ac.uk</a>)
+  </p>
+</div>
+
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
